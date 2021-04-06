7a13127f693149dd5eac893dd829e2ec9235467e
saskamegaprogrammist <spiridonovaalex@gmail.com>
2021-04-06 19:38:51 +0300

    test2

--- simplex_methods/simplex_method.go
+++ simplex_methods/simplex_method.go
@@ -239,7 +239,8 @@ func (sm *SimplexMethod) Solve() ([]float64, float64, error) {
 }
 
 func (sm *SimplexMethod) findMin(bVecNew la_methods.Vector, aVecNew la_methods.Vector) (bool, float64, int) {
-	var min = float64(100000000000)
+	var min = float64(1000000000001234)
+	fmt.Print("test2")
 	var has bool
 	var minI int
 	var val float64
