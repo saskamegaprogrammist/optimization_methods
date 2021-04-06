a691fdffd557333ae7897550a5a5c750173fb727
saskamegaprogrammist <spiridonovaalex@gmail.com>
2021-04-06 19:40:35 +0300

    Revert "Merge branch 'test2' into test"
    This reverts commit d8a1cf60a76719e3768be20e02272592f03d5991, reversing
    changes made to f4ed313c69a733717da1c5707edb61add228ee1d.

--- simplex_methods/simplex_method.go
+++ simplex_methods/simplex_method.go
@@ -239,8 +239,8 @@ func (sm *SimplexMethod) Solve() ([]float64, float64, error) {
 }
 
 func (sm *SimplexMethod) findMin(bVecNew la_methods.Vector, aVecNew la_methods.Vector) (bool, float64, int) {
-	var min = float64(1000000000001234)
-	fmt.Print("test2")
+	var min = float64(100234000000000)
+	fmt.Print("test")
 	var has bool
 	var minI int
 	var val float64
